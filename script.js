88836bc9ce38715ab3c9838ae297c03afb6a9965
cgarnica92 <c_garnica92@yahoo.com>
2023-01-08 22:29:03 -0500

    Started API Call/ Forecast Logic

--- script.js
+++ script.js
@@ -3,18 +3,16 @@ var searchHistory = [];
 var weatherApiRootUrl = 'https://api.openweathermap.org';
 var weatherApiKey = 'd91f911bcf2c0f925fb6535547a5ddc9';
 
-//DOM Variables
+
 
 
 dayjs.extend(window.dayjs_plugin_utc);
 dayjs.extend(window.dayjs_plugin_timezone);
 
-function renderForeCard(forecast) {
+function renderForecastCard(forecast) {
     var iconUrl = `https://openweathermap.org/img/w/${forecast.weather[0].icon}.png`;
     var iconDescription = forecast.weather[0].description;
     var tempF = forecast.main.temp;
     var humidity = forecast.main.humidity;
     var windMph = forecast.wind.speed;
-
-
 }
