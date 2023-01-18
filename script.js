bd0d42d0c55810a5599e21a5916754783a70666b
cgarnica92 <c_garnica92@yahoo.com>
2023-01-17 20:58:00 -0500

    Search Functions, Calls API

--- script.js
+++ script.js
@@ -3,16 +3,56 @@ var searchHistory = [];
 var weatherApiRootUrl = 'https://api.openweathermap.org';
 var weatherApiKey = 'd91f911bcf2c0f925fb6535547a5ddc9';
 
+var searchForm = document.querySelector('#search-form');
+var searchInput = document.querySelector('#search-input');
 
+function fetchWeather(location) {
+    var { lat } = location;
+    var { lon } = location;
+    var city = location.name;
+  
+    var apiUrl = `${weatherApiRootUrl}/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${weatherApiKey}`;
+  
+    fetch(apiUrl)
+      .then(function (res) {
+        return res.json();
+      })
+      .then(function (data) {
+        console.log(city, data);
+      })
+      .catch(function (err) {
+        console.error(err);
+      });
+  }
 
+function fetchCoords(search) {
+    var apiUrl = `${weatherApiRootUrl}/geo/1.0/direct?q=${search}&limit=5&appid=${weatherApiKey}`;
+  
+    fetch(apiUrl)
+      .then(function (res) {
+        return res.json();
+      })
+      .then(function (data) {
+        if (!data[0]) {
+          alert('Location not found');
+        } else {
+          fetchWeather(data[0]);
+        }
+      })
+      .catch(function (err) {
+        console.error(err);
+      });
+  }
 
-dayjs.extend(window.dayjs_plugin_utc);
-dayjs.extend(window.dayjs_plugin_timezone);
+function handleSearchFormSubmit(e) {
+    if (!searchInput.value) {
+      return;
+    }
+  
+    e.preventDefault();
+    var search = searchInput.value.trim();
+    fetchCoords(search);
+    searchInput.value = '';
+  }
 
-function renderForecastCard(forecast) {
-    var iconUrl = `https://openweathermap.org/img/w/${forecast.weather[0].icon}.png`;
-    var iconDescription = forecast.weather[0].description;
-    var tempF = forecast.main.temp;
-    var humidity = forecast.main.humidity;
-    var windMph = forecast.wind.speed;
-}
+  searchForm.addEventListener('submit', handleSearchFormSubmit);
