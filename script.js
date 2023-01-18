653f173b7e1eef624f2ca2a65a8afd0d572fb674
cgarnica92 <c_garnica92@yahoo.com>
2023-01-17 21:42:43 -0500

    Renders Weather

--- script.js
+++ script.js
@@ -5,6 +5,32 @@ var weatherApiKey = 'd91f911bcf2c0f925fb6535547a5ddc9';
 
 var searchForm = document.querySelector('#search-form');
 var searchInput = document.querySelector('#search-input');
+var weatherContainer = document.querySelector('#weather-container')
+
+dayjs.extend(window.dayjs_plugin_utc);
+dayjs.extend(window.dayjs_plugin_timezone);
+
+function renderCurrentWeather(city, weather) {
+    var date = dayjs().format('M/D/YYYY');
+    var tempF = weather.main.temp;
+    var windMph = weather.wind.speed;
+    var humidity = weather.main.humidity;
+
+    var card = document.createElement('div');
+    var heading = document.createElement('h2');
+    var tempEl = document.createElement('p');
+    var windEl = document.createElement('p');
+    var humidityEl = document.createElement('p');
+
+    heading.textContent = `${city} (${date})`;
+    tempEl.textContent = `Temp: ${tempF}°F`;
+    windEl.textContent = `Wind: ${windMph} MPH`;
+    humidityEl.textContent = `Humidity: ${humidity} %`;
+    card.append(heading, tempEl, windEl, humidityEl);
+
+    weatherContainer.innerHTML = '';
+    weatherContainer.append(card);
+}
 
 function fetchWeather(location) {
     var { lat } = location;
@@ -18,7 +44,7 @@ function fetchWeather(location) {
         return res.json();
       })
       .then(function (data) {
-        console.log(city, data);
+        renderCurrentWeather(city, data.list[0], data.city.timezone);
       })
       .catch(function (err) {
         console.error(err);
